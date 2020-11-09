235f6c1490cc8e28e80de08cdb2253aad841e880
Ahmed Salem <ahmed.salem5564@gmail.com>
2020-11-09 18:07:00 +0200

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/98005543

---
+++ codeforces/282/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//===================== Salem ============================
+
+#define forn(i,n)  for (int i = 0; i < (int)(n); ++i)
+#define for1(i,n)  for (int i = 1; i <=(int)(n); ++i)
+#define ulli       unsigned long long int
+#define ull        unsigned long long
+#define lli        long long int
+#define ll         long long
+#define all(v)     (v).begin(),(v).end()
+#define PI         3.14159265359
+#define pb         push_back
+#define pp         pop_back
+
+//========================================================
+void FastInputOutput()
+{
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    // cout << fixed << setprecision(10);
+    // freopen("input.txt","r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //transform(su.begin(), su.end(), su.begin(), ::toupper);
+
+
+    int n, x = 0 ;  cin>>n;
+    string s;
+    forn (i, n)
+    {
+        cin>>s;
+        if (s[1] == '+') ++x;
+        else --x;
+    }
+    cout<<x;
+}
