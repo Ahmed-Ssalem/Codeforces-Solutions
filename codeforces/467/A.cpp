39ac10fa79b9cca1ad65426f93c40b17f66b72ee
Ahmed Salem <ahmed.salem5564@gmail.com>
2020-11-26 13:07:00 +0200

    Add solution for problem `A - George and Accommodation` Link: https://codeforces.com/contest/467/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/467/submission/99632151

---
+++ codeforces/467/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//===================== Salem ============================
+
+#define forn(i,n)  for (int i = 0; i < (int)(n); ++i)
+#define for1(i,n)  for (int i = 1; i <=(int)(n); ++i)
+#define ulli       unsigned long long int
+#define ull        unsigned long long
+#define lli        long long int
+#define ll         long long
+#define all(v)     (v).begin(),(v).end()
+#define PI         3.14159265359
+#define pb         push_back
+#define pp         pop_back
+#define PI         3.14159265359
+#define el         endl
+const int mod = 1e9+7;
+const ll inf = 1e18;
+
+//========================================================
+void FastInputOutput()
+{
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main()
+{
+    // cout << fixed << setprecision(10);
+    //freopen("contest.in","r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //transform(su.begin(), su.end(), su.begin(), ::toupper);
+
+	int n;  cin>>n;
+	n *= 2;
+	int arr[n], count = 0;
+	forn (i, n)
+	{
+		cin>>arr[i];
+	}
+	for (int i = 0; i < n-1; i += 2)
+	{
+		if (arr[i] + 2 <= arr[i+1] ) count++;
+	}
+	cout<<count;
+    return 0;
+}
