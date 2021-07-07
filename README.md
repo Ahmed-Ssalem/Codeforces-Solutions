ee2889e828c981a009e7c925434430f2622fa309
Ahmed Salem <ahmed.salem5564@gmail.com>
2021-07-07 21:28:00 +0200

    Add solution for problem `A - Design Tutorial: Learn from Math` Link: https://codeforces.com/contest/472/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/472/submission/121659359

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-2 | [B - Ania and Minimizing](https://codeforces.com/contest/1230/problem/B) | [GNU C++17](./codeforces/1230/B.cpp) | `greedy` `implementation` `*1000` | Dec/22/2021 19:19 | 
-1 | [D - Dima and Lisa](https://codeforces.com/contest/584/problem/D) | [GNU C++11](./codeforces/584/D.cpp) | `brute force` `math` `number theory` `*1800` | Jul/08/2021 05:23 | 
+3 | [B - Ania and Minimizing](https://codeforces.com/contest/1230/problem/B) | [GNU C++17](./codeforces/1230/B.cpp) | `greedy` `implementation` `*1000` | Dec/22/2021 19:19 | 
+2 | [D - Dima and Lisa](https://codeforces.com/contest/584/problem/D) | [GNU C++11](./codeforces/584/D.cpp) | `brute force` `math` `number theory` `*1800` | Jul/08/2021 05:23 | 
+1 | [A - Design Tutorial: Learn from Math](https://codeforces.com/contest/472/problem/A) | [GNU C++11](./codeforces/472/A.cpp) | `math` `number theory` `*800` | Jul/07/2021 21:28 | 
---
+++ codeforces/472/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//===================== Salem ============================
+
+#define forn(i,n)  for (int i = 0; i < (int)(n); ++i)
+#define for1(i,n)  for (int i = 1; i <= (int)(n); ++i)
+#define ulli       unsigned long long int
+#define ull        unsigned long long
+#define lli        long long int
+#define ll         long long
+#define all(v)     (v).begin(),(v).end()
+#define allr(v)    (v).rbegin(),(v).rend()
+#define sz(v)      (int)((v).size())
+#define PI         3.14159265359
+#define pb         push_back
+#define pp         pop_back
+#define el         endl
+const int mod = 1e9 + 7;
+const ll inf = 1e18;
+
+//========================================================
+void FastInputOutput() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int main() {
+	//cout << fixed << setprecision(10);
+	//freopen("contest.in","r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//trvform(su.begin(), su.end(), su.begin(), ::toupper);
+	/*list <int> :: iterator it = l.begin();
+	 *for ( ;it != l.end(); ++it)
+	 *    cout<<*it<<" ";
+	 */
+
+	int n;
+	cin >> n;
+	if (n % 2 == 0)
+		cout << n - 8 << " " << 8;
+	else
+		cout << n - 9 << " " << 9;
+
+	return 0;
+}
